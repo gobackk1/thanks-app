bc23a2451b6bc7527bd91929491941383071c67e
gobackk1 <k.kwbt@icloud.com>
2020-11-22 11:38:43 +0900

    init

---
+++ src/index.tsx
@@ -0,0 +1,4 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(<React.StrictMode>check</React.StrictMode>, document.getElementById('root'))
---
+++ webpack.config.js
@@ -0,0 +1,104 @@
+const Dotenv = require('dotenv-webpack')
+const path = require('path')
+const fs = require('fs')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+const appDirectory = fs.realpathSync(process.cwd())
+const resolveApp = relativePath => path.resolve(appDirectory, relativePath)
+
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production' ? true : false
+  return {
+    entry: './src/index.tsx',
+    output: {
+      filename: 'bundle.min.js',
+      path: isProduction ? resolveApp('build') : undefined,
+      publicPath: '/'
+    },
+    module: {
+      rules: [
+        {
+          test: [/\.tsx?$/],
+          loader: 'babel-loader',
+          exclude: [/node_modules/, /test/],
+          options: {
+            presets: [
+              ['@babel/preset-env', { useBuiltIns: 'usage', corejs: 3 }],
+              '@babel/preset-react',
+              '@babel/preset-typescript'
+            ],
+            plugins: ['@babel/plugin-proposal-class-properties']
+          }
+        },
+        {
+          test: /\.tsx?$/,
+          loader: 'ts-loader',
+          exclude: [/node_modules/, /test/]
+        },
+        {
+          test: /\.tsx?$/,
+          loader: 'eslint-loader',
+          exclude: [/node_modules/, /test/],
+          include: resolveApp(`src`),
+          options: {
+            eslintPath: 'eslint'
+          }
+        },
+        {
+          test: /\.(jpg|png|gif)$/,
+          loader: 'file-loader',
+          options: {
+            name: 'images/[name].[hash:8].[ext]'
+          }
+        }
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: resolveApp('public/index.html'),
+        minify: {
+          removeComments: true,
+          collapseWhitespace: true,
+          removeRedundantAttributes: true,
+          useShortDoctype: true,
+          removeEmptyAttributes: true,
+          removeStyleLinkTypeAttributes: true,
+          keepClosingSlash: true,
+          minifyJS: true,
+          minifyCSS: true,
+          minifyURLs: true
+        }
+      }),
+      new Dotenv()
+    ],
+    resolve: {
+      alias: {
+        '@': resolveApp('src'),
+        '~redux': resolveApp('src/scripts/redux')
+      },
+      extensions: ['.ts', '.tsx', '.js', '.jsx']
+    },
+    ...(isProduction
+      ? {
+          optimization: {
+            minimize: true,
+            minimizer: [
+              new TerserPlugin({
+                terserOptions: {
+                  compress: {
+                    drop_console: true
+                  }
+                }
+              })
+            ]
+          }
+        }
+      : {
+          devtool: 'source-map',
+          devServer: {
+            contentBase: resolveApp('public'),
+            historyApiFallback: true
+          }
+        })
+  }
+}
