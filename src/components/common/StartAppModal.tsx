0b36f755c787d53f7bf060b1d3ed07c4dbe07f2e
gobackk1 <k.kwbt@icloud.com>
2020-11-23 11:46:40 +0900

    ユーザー登録するモーダル実装

--- src/components/common/StartAppModal.tsx
+++ src/components/common/StartAppModal.tsx
@@ -1,11 +1,41 @@
 import React from 'react'
-import { Button } from '@material-ui/core'
+import { Button, TextField } from '@material-ui/core'
 import { AppModal } from '@/components'
+import { useForm } from 'react-hook-form'
+import { useFirebase } from '@/hooks'
 
 export const StartAppModal: React.FC = () => {
+  const { register, handleSubmit, errors } = useForm()
+  const { signup } = useFirebase()
+
   return (
     <AppModal title="始める" renderButton={props => <Button {...props}>button</Button>}>
-      ここにフォーム
+      <form onSubmit={handleSubmit(signup)}>
+        <TextField
+          name="email"
+          inputRef={register}
+          error={!!errors.email}
+          type="email"
+          label="メールアドレス"
+          helperText={errors.email?.message}
+          variant="filled"
+          required
+          fullWidth
+        />
+        <br />
+        <TextField
+          name="password"
+          inputRef={register}
+          error={!!errors.password}
+          type="password"
+          label="パスワード"
+          helperText={errors.password?.message}
+          variant="filled"
+          required
+          fullWidth
+        />
+        <Button type="submit">登録</Button>
+      </form>
     </AppModal>
   )
 }
