98e9f46b44025ee7feaea40a81960d94bd390d79
gobackk1 <k.kwbt@icloud.com>
2020-11-23 11:51:47 +0900

    url変更/組織のスペース作成機能実装

--- src/hooks/useFirebase.ts
+++ src/hooks/useFirebase.ts
@@ -1,6 +1,9 @@
 import React from 'react'
 import firebase from 'firebase'
 import { useSnackbarContext } from '@/hooks'
+import * as T from '@/model/types'
+import * as LoginUser from '@/context/LoginUserContext'
+import { useHistory } from 'react-router-dom'
 
 type CreateUserFormValue = {
   email: string
@@ -9,16 +12,21 @@ type CreateUserFormValue = {
 
 type ReturnType = {
   signup: (value: CreateUserFormValue) => void
+  createCompany: (name: string) => Promise<void>
 }
+
+const db = firebase.firestore()
 
 export const useFirebase = (): ReturnType => {
   const { showSnackbar } = useSnackbarContext()
+  const [state, setState] = React.useContext(LoginUser.Context)
+  const history = useHistory()
 
   const signup = React.useCallback(
     async ({ email, password }: CreateUserFormValue) => {
       const credit = await firebase.auth().createUserWithEmailAndPassword(email, password)
       await credit.user?.sendEmailVerification({
-        url: 'http://localhost'
+        url: 'http://localhost:8080/registration'
       })
       showSnackbar({
         message: '登録メールアドレスに確認メールを送信しました。',
@@ -29,5 +37,20 @@ export const useFirebase = (): ReturnType => {
     [showSnackbar]
   )
 
-  return { signup }
+  const createCompany = React.useCallback(
+    async (name: string) => {
+      const company: T.Company = {
+        name,
+        profile: 'プロフィールを入力してください',
+        // 初期画像探す
+        avatarURL: 'default'
+      }
+      await db.collection('company').add(company)
+      setState(state => ({ ...state, company }))
+      history.push('company')
+    },
+    [setState, history]
+  )
+
+  return { signup, createCompany }
 }
