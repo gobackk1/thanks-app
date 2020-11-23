32a8c67b68af172acdc19ce454d7fe75a58a356c
gobackk1 <k.kwbt@icloud.com>
2020-11-23 11:51:06 +0900

    ログインユーザーの状態に応じて出し分ける

--- src/components/view/PageLayout.tsx
+++ src/components/view/PageLayout.tsx
@@ -1,19 +1,10 @@
 import React from 'react'
-import {
-  AppBar,
-  Toolbar,
-  Drawer,
-  List,
-  ListItemIcon,
-  ListItem,
-  ListItemText,
-  makeStyles,
-  CssBaseline
-} from '@material-ui/core'
+import { Drawer, List, ListItemIcon, ListItem, ListItemText, makeStyles } from '@material-ui/core'
 import HomeRoundedIcon from '@material-ui/icons/HomeRounded'
 import RedeemRoundedIcon from '@material-ui/icons/RedeemRounded'
 import { useHistory } from 'react-router-dom'
-
+import * as LoginUser from '@/context/LoginUserContext'
+import { useAuthentication } from '@/hooks'
 const list = [
   { text: 'ホーム', icon: <HomeRoundedIcon />, path: '/' },
   { text: 'リワード', icon: <RedeemRoundedIcon />, path: '/rewards' }
@@ -22,28 +13,31 @@ const list = [
 export const PageLayout: React.FC = ({ children }) => {
   const styles = useStyles()
   const history = useHistory()
-
+  useAuthentication()
+  const [state] = React.useContext(LoginUser.Context)
+  console.log(state.uid)
   return (
     <div className={styles.root}>
-      {/* <AppBar position="fixed">
-        <Toolbar></Toolbar>
-      </AppBar> */}
       <Drawer
         className={styles.drawer}
         classes={{ paper: styles.drawerPaper }}
         variant="permanent"
         anchor="left"
       >
-        <div>check</div>
-        <CssBaseline />
-        <List>
-          {list.map(({ text, icon, path }, i) => (
-            <ListItem button key={i} onClick={() => history.push(path)}>
-              <ListItemIcon>{icon}</ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List>
+        {!state.uid && 'ログインボタンをここに置く'}
+        {state.company ? (
+          <List>
+            {list.map(({ text, icon, path }, i) => (
+              // currentuser contextを作って出し分ける
+              <ListItem button key={i} onClick={() => history.push(path)}>
+                <ListItemIcon>{icon}</ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItem>
+            ))}
+          </List>
+        ) : (
+          '組織を選択してください'
+        )}
       </Drawer>
       <main className={styles.main}>{children}</main>
     </div>
