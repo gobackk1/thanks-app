458ce8fdfc116528824ffffb545afcab59e909e5
gobackk1 <k.kwbt@icloud.com>
2020-11-25 10:35:45 +0900

    createUser追加

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,21 +2,19 @@ import * as functions from 'firebase-functions'
 import * as admin from 'firebase-admin'
 import * as serviceAccount from './serviceAccount.json'
 import { HttpsError } from 'firebase-functions/lib/providers/https'
-import firebase from 'firebase/app'
+// import firebase from 'firebase'
 
 admin.initializeApp({
   databaseURL: 'https://thanks-app-d91d5.firebaseio.com',
   credential: admin.credential.cert(serviceAccount as admin.ServiceAccount)
 })
 
-const db = firebase.firestore()
-
 export const onCreateUser = functions
   .region('asia-northeast1')
   .auth.user()
   .onCreate(async user => {
     /**
-     * NOTE: 後悔するユーザーの情報は user ドキュメントに持たせる
+     * NOTE: 公開するユーザーの情報は user ドキュメントに持たせる
      */
     const userdata = {
       name: 'userName',
@@ -46,7 +44,6 @@ type SetAdminUserParams = {
   uid: string
   password: string
 }
-
 export const setAdminUser = functions
   .region('asia-northeast1')
   .https.onCall(async (data: SetAdminUserParams, context) => {
@@ -57,10 +54,23 @@ export const setAdminUser = functions
         admin: data.isAdmin
       }
       await admin.auth().setCustomUserClaims(data.uid, customClaims)
-      await db
+      await admin
+        .firestore()
         .collection('users')
         .doc(data.uid)
-        .update({ isAdmin: data.isAdmin })
+        .set({ isAdmin: data.isAdmin }, { merge: true })
+      return { message: 'success' }
+    } catch (error) {
+      return { error, data }
+    }
+  })
+
+export const createUser = functions
+  .region('asia-northeast1')
+  .https.onCall(async (data, context) => {
+    if (!context.auth) return { error: 'Not authorized.' }
+    try {
+      await admin.auth().createUser({ email: data.email, password: data.password })
       return { message: 'success' }
     } catch (error) {
       return { error }
