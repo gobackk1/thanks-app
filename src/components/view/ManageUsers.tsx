7371f843cb95ba9f37b05191d95fd234bce12bd8
gobackk1 <k.kwbt@icloud.com>
2020-11-26 17:33:45 +0900

    リファクタ

--- src/components/view/ManageUsers.tsx
+++ src/components/view/ManageUsers.tsx
@@ -9,53 +9,17 @@ import {
   IconButton
 } from '@material-ui/core'
 import React from 'react'
-import * as T from '@/model/types'
-import { Visibility } from '@material-ui/icons'
 import { CreateUserModal } from '@/components'
-import firebase from 'firebase/app'
 import HighlightOffRoundedIcon from '@material-ui/icons/HighlightOffRounded'
 import { useFirebase } from '@/hooks'
 import EditRoundedIcon from '@material-ui/icons/EditRounded'
 import * as UpdateUserModal from '@/context/UpdateUserModalContext'
-import { useSelector } from 'react-redux'
 import { useUsersState } from '@/hooks/useUsersState'
 
-type User = T.User & { uid: string }
-
 export const ManageUsers: React.FC = () => {
-  const users = useSelector(state => state.users)
   const { deleteUser } = useFirebase()
   const { openUpdateUserModal } = React.useContext(UpdateUserModal.Context)
   const { getUsers } = useUsersState()
-
-  React.useEffect(() => {
-    // const unsubscribe = firebase
-    //   .firestore()
-    //   .collection('users')
-    //   .onSnapshot(querySnapshot => {
-    //     for (const change of querySnapshot.docChanges()) {
-    //       if (change.type === 'added') {
-    //         setUsers(users => [...users, { ...change.doc.data(), uid: change.doc.id } as User])
-    //       } else if (change.type === 'modified') {
-    //         setUsers(users =>
-    //           users.map(user => {
-    //             const changeData = change.doc.data() as T.User
-    //             const uid = change.doc.id
-    //             if (user.uid === uid) {
-    //               return { ...changeData, uid }
-    //             }
-    //             return user
-    //           })
-    //         )
-    //       } else if (change.type === 'removed') {
-    //         setUsers(users => users.filter(user => user.email !== change.doc.data().email))
-    //       }
-    //     }
-    //   })
-    // return () => {
-    //   unsubscribe()
-    // }
-  }, [])
 
   return (
     <>
@@ -77,12 +41,7 @@ export const ManageUsers: React.FC = () => {
                 <TableCell>{user.email}</TableCell>
                 <TableCell>{user.isAdmin ? '管理者' : '-'}</TableCell>
                 <TableCell>
-                  <IconButton
-                    onClick={() => {
-                      openUpdateUserModal(user)
-                      console.log('buttonClick', user.uid)
-                    }}
-                  >
+                  <IconButton onClick={() => openUpdateUserModal(user)}>
                     <EditRoundedIcon />
                   </IconButton>
                   <IconButton onClick={() => deleteUser(user.uid)}>
