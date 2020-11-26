67886fb1ddbb12d35915dadf73f506abed92f4fd
gobackk1 <k.kwbt@icloud.com>
2020-11-26 09:05:23 +0900

    よく合わせて利用するので

--- src/redux/actionCreator.ts
+++ src/redux/actionCreator.ts
@@ -1,8 +1,11 @@
 import { actionCreatorFactory } from 'typescript-fsa'
 import { asyncFactory } from 'typescript-fsa-redux-thunk'
+import firebase from 'firebase/app'
 
 /**
  * 各アクションで利用するファクトリメソッド
  */
 export const actionCreator = actionCreatorFactory()
 export const asyncActionCreator = asyncFactory(actionCreator)
+
+export const db = firebase.firestore()
